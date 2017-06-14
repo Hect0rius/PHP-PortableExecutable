982a305f31e084427b399e8970a7958e21d1f59c
Hect0r Xorius <sysop@pkx.pw>
2017-06-14 09:52:17 +0100

    Updated help output.

--- info.php
+++ info.php
@@ -8,6 +8,13 @@
 include('io/FileStream.php');
 include('pe/Binary.php');
 
+if(!isset($argv[1]) || $argv[1] === 'help') { 
+    echo "PE Info - Reads the structural informationof the PE.\n" . 
+         "Usage: php -f info.php [peInputPath]\n" . 
+         "Example: php -f info.php /home/user/executable.exe\n";
+    die();
+}
+
 // Check File Exists.
 if($argc < 2 || !file_exists($argv[1])) {
     die("Unable to find a executable to meddle with :(\n");
